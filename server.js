a6036f3bc63f98b393ef67d74eee1bf4326546ab
danimohli <danimohlife@gmail.com>
2024-12-11 21:39:35 +0100

    server

--- server.js
+++ server.js
@@ -1,11 +1,15 @@
-const express = require('express');
-const routes = require('./routes');
+import express from 'express';
+import controllerRouting from './routes/index';
 
 const app = express();
 const port = process.env.PORT || 5000;
 
-app.use('/', routes);
+app.use(express.json());
+
+controllerRouting(app);
 
 app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
+  console.log(`Server running on port ${port}`);
 });
+
+export default app;
